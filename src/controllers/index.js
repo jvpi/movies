628ec81c8cb6436c4de5c62c0513a268ed721829
jesus <jvpi189@gmail.com>
2023-11-01 21:49:42 -0400

    cambiando pelicula

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,11 @@ const indexController = {}
 indexController.renderIndex = function(req, res, next) {
     res.render('index');
 }
-
+indexController.subirArchivos = function (req, res) {
+    console.log(req.file);
+    res.redirect('/')
+}
+indexController.video = function(req, res, next) {
+    res.send('hola');
+}
 module.exports = indexController
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/index.js')
 
+let multer = require('multer')
+let storage = multer.diskStorage({
+    destination:function (req,file,cb) {
+        cb(null,'cargas')
+    },
+    filename:function (req,file,cb) {
+        cb(null,`${file.originalname}`)
+    }
+})
+let carga = multer({
+    storage:storage
+})
 router.get('/',indexController.renderIndex);
-
+router.get('/video',indexController.video);
+router.post('/',carga.single('avatar'),  indexController.subirArchivos);
 module.exports = router;
+//
